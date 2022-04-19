30453a055e0917f800cd4f03b1fd0fe8cdc9338b
sam-masaki <samuel26@cs.uw.edu>
2022-04-19 16:51:22 -0700

    follow project format

--- src/playback.rs
+++ src/playback.rs
@@ -1,11 +1,11 @@
 use super::analyze;
 
 pub struct Playhead {
-    pub time: i32,
-    pub idxs: [usize; 10],
+  pub time: i32,
+  pub idxs: [usize; 10],
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 }
