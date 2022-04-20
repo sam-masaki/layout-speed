12cd268d2e630116957e2fde3aacfd575e35dc44
sam-masaki <samuel26@cs.uw.edu>
2022-04-19 19:55:18 -0700

    Avoid creating duplicate keyframes

--- src/analyze.rs
+++ src/analyze.rs
@@ -11,6 +11,8 @@ pub struct Keyframe {
   on_char: char,
 }
 
+static PRESS_DUR: i32 = 250;
+
 pub fn gen_timeline<'a>(string: &str, lay: &'a layout::Layout) -> Timeline {
   // May want a different data structure for generating vs playback
   let mut fingers = [
@@ -51,58 +53,66 @@ pub fn gen_timeline<'a>(string: &str, lay: &'a layout::Layout) -> Timeline {
     let home_key = lay.homes[findex];
     let prev_frame = fingers[findex].last().unwrap();
 
-    let start_move_time = if min_press > prev_frame.time {
+    let start_move_dur = move_time(&prev_frame.pos, &key.pos);
+    let end_move_dur = move_time(&key.pos, &home_key.pos);
+
+    let time_start_move = if min_press > prev_frame.time {
       min_press
     } else {
       prev_frame.time
     };
-    let start_press_time = start_move_time + move_time(&prev_frame.pos, &key.pos);
-    let end_press_time = start_press_time + 250;
-    let end_move_time = end_press_time + move_time(&key.pos, &home_key.pos);
 
-    let start_move = Keyframe {
-      pos: layout::Pos {
-        x: prev_frame.pos.x,
-        y: prev_frame.pos.y,
-      },
-      time: start_move_time,
-      start_press: false,
-      on_char: prev_frame.on_char,
-    };
+    if time_start_move != prev_frame.time {
+      let start_move = Keyframe {
+        pos: layout::Pos {
+          x: prev_frame.pos.x,
+          y: prev_frame.pos.y,
+        },
+        time: time_start_move,
+        start_press: false,
+        on_char: prev_frame.on_char,
+      };
+      fingers[findex].push(start_move);
+    }
+
+    let time_start_press = time_start_move + start_move_dur;
+    let time_end_press = time_start_press + PRESS_DUR;
+    let time_end_move = time_end_press + end_move_dur;
+
     let start_press = Keyframe {
       pos: layout::Pos {
         x: key.pos.x,
         y: key.pos.y,
       },
-      time: start_press_time,
+      time: time_start_press,
       start_press: true,
       on_char: key.pressed,
     };
+    fingers[findex].push(start_press);
+
     let end_press = Keyframe {
       pos: layout::Pos {
         x: key.pos.x,
         y: key.pos.y,
       },
-      time: end_press_time,
+      time: time_end_press,
       start_press: false,
       on_char: key.pressed,
     };
+    fingers[findex].push(end_press);
+
     let end_move = Keyframe {
       pos: layout::Pos {
         x: home_key.pos.x,
         y: home_key.pos.y,
       },
-      time: end_move_time,
+      time: time_end_move,
       start_press: false,
       on_char: home_key.pressed,
     };
-
-    fingers[findex].push(start_move);
-    fingers[findex].push(start_press);
-    fingers[findex].push(end_press);
     fingers[findex].push(end_move);
 
-    min_press = end_press_time;
+    min_press = time_end_press;
   }
 
   Timeline { fingers }
@@ -126,7 +136,8 @@ fn move_time(start: &layout::Pos, end: &layout::Pos) -> i32 {
 
   let dist = (x_diff.powi(2) + y_diff.powi(2)).sqrt();
 
-  std::cmp::max((dist * 250.0) as i32, 250)
+  //  std::cmp::max((dist * 250.0) as i32, 250)
+  (dist * 250.0) as i32
 }
 
 #[cfg(test)]
@@ -141,6 +152,9 @@ mod tests {
         assert!(kf.time >= prev_time, "A keyframe goes backwards in time");
         prev_time = kf.time;
 
+        // FIXME: This doesn't work because it's going through each
+        // finger individually. Need a utility function for turning a
+        // Timeline into a flat list of KeyFrames
         if kf.start_press {
           assert_eq!(
             kf.on_char,
@@ -162,5 +176,27 @@ mod tests {
     let text = "rgvf";
     let tl = gen_timeline(text, lay);
     common_invariants(&tl, text);
+  }
+
+  #[test]
+  fn moveless_text() {
+    // Test text that's all on the home row
+    let mut lay = layout::Layout::default();
+    let lay = layout::init(&mut lay, "qwerty.layout").unwrap();
+
+    let text = "asdf jkl;";
+    let tl = gen_timeline(text, lay);
+    // TODO: after fixing, check this again
+    //common_invariants(&tl, text);
+
+    let mut prev_press_end = 0;
+    for i in 0..10 {
+      for kf in &tl.fingers[i] {
+        if kf.start_press {
+          assert_eq!(prev_press_end, kf.time);
+          prev_press_end = kf.time + PRESS_DUR;
+        }
+      }
+    }
   }
 }
